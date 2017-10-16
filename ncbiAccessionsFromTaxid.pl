f2a5ebaa943e6f4b9f481850b79a51eec6f50434
Marc-Olivier Duceppe <marc-olivier.duceppe@inspection.gc.ca>
2017-10-16 11:40:23 -0400

    Added options, removed Forkmanager

--- ncbiAccessionsFromTaxid.pl
+++ ncbiAccessionsFromTaxid.pl
@@ -10,31 +10,62 @@
 
 use LWP::Simple;
 use Term::ProgressBar;
-use Sys::CPU;
-use Parallel::ForkManager;
+
 use strict;
 use warnings;
 use diagnostics;
 use Data::Dumper;
 
+use Getopt::Long;
+use Pod::Usage;
 
-#Check required arguments
-if (scalar(@ARGV) != 2)
+
+####################
+#                  #
+#   Option flags   #
+#                  #
+####################
+
+
+# Default values
+my $db;
+my $query;
+my $outfile;
+my $help;
+my $man;
+
+
+GetOptions(
+    'db|d=s' => \$db,  # string
+    'query|q=s' => \$query,  # string
+    'output|o=s' => \$outfile,  # string
+    'help|h' => \$help,
+    'man' => \$man) or pod2usage(2);
+
+pod2usage(1) if $help;
+pod2usage(-exitval => 0, -verbose => 2) if $man;
+
+
+##############
+#            #
+#   Checks   #
+#            #
+##############
+
+
+#check if arguments are not empty
+if (!defined($db) || !defined($query) || !defined($outfile))
 {
-    print "Usage: perl ncbiAccessionsFromTaxid.pl <query> <accession.list>\n\n";
-    #perl ncbiAccessionsFromTaxid.pl "txid1639[Organism:exp]" ~/Desktop/listeria_mono.list
-    print "Example: perl ncbiAccessionsFromTaxid.pl \"txid1639[Organism:exp]\" ~/Desktop/listeria_mono.list";
-    exit;
+    pod2usage(1)
 }
 
-#print(Dumper \@ARGV);
 
-#Command line arguments
-my $query = $ARGV[0];
-my $outfile = $ARGV[1];
+############
+#          #
+#   Main   #
+#          #
+############
 
-#select database
-my $db = "protein";
 
 #assemble the esearch URL
 my $base = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/';
@@ -53,49 +84,70 @@ my $count = $1 if ($output =~ /<Count>(\d+)<\/Count>/);
 print "There is $count $db accessions for $query";
 
 #open output file for writing
-open(OUT, ">>", $outfile) || die "Can't open file!\n";
+open(OUT, ">", $outfile) || die "Can't open file!\n";
 
 #progress bar
 print "\n\nDownloading list of accessions\n\n";
-#my $progress = Term::ProgressBar->new($count);
+my $progress = Term::ProgressBar->new($count);
 
 # https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=protein&WebEnv=NCID_1_2591324_130.14.22.215_9001_1469634077_1008305355_0MetA0_S_MegaStore_F_1&query_key=1&&retmax=500&&rettype=acc
 #create all the URLs to retrieve and store them in an array
 #retrieve data in batches of 200
 my $retmax = 500;
-my @URLs;
 for (my $retstart = 0; $retstart < $count; $retstart += $retmax) {
         my $efetch_url = $base ."efetch.fcgi?db=$db&WebEnv=$web";
         $efetch_url .= "&query_key=$key&retstart=$retstart";
         $efetch_url .= "&retmax=$retmax&rettype=acc&retmode=text";
-        #my $efetch_out = get($efetch_url);
-        #print OUT "$efetch_out";
-        
-        push(@URLs, $efetch_url);
+        my $efetch_out = get($efetch_url);
+        print OUT "$efetch_out";
         
         #update progress bar
-        #$progress->update($_);
+        $progress->update($_);
 }
-
-#Use parallel loop to download multple URLs at the same time
-
-#Prepare for parallel loop
-my $number_of_cpus = Sys::CPU::cpu_count();
-my $MAX_PROCESSES = $number_of_cpus;
-my $pm = Parallel::ForkManager->new($MAX_PROCESSES);
-
-foreach my $link (@URLs)
-{
-    $pm-> start and next; # Forks and returns the pid for the child
-        
-    my $efetch_out = get($link);
-    if ($efetch_out) { print OUT $efetch_out; }
-    else { print "Could not download accessions from $link\n"; }
-    
-    $pm->finish; # terminate the child process
-}
-$pm->wait_all_children;
 
 close OUT;
 
-print ("\n\ndone\n");
+print ("\ndone\n");
+
+
+__END__
+=head1 NAME
+
+ncbiAccessionsFromTaxid.pl - Get the accession numbers related to the result of the text query
+
+=head1 SYNOPSIS
+
+perl ncbiAccessionsFromTaxid.pl --db <protein|nucleotide> --query 'my query' --output <accession.list>
+
+=head1 OPTIONS
+
+=over 4
+
+=item B<--help [-h]>
+
+Print this help
+
+=item B<--man>
+
+Print script manual
+
+=item B<--db [-d]>
+
+NCBI database. Allowed values are 'protein' or 'nucleotide'
+
+=item B<--query [-q]>
+
+String to look for. E.g. 'txid33090[Organism] AND "internal transcribed spacer"'
+
+=item B<--output [-o]>
+
+Output file. This is the list of accession numbers.
+
+=back
+
+=head1 DESCRIPTION
+
+B<ncbiAccessionsFromTaxid.pl> will search a query string on NCBI and return a list of corresponding nucleotide or protein accession numbers,
+depending which database is selected.
+
+=cut
